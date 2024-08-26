7120b76ff8ea8eb16b6e371b53eb9e2150965865
Yumiko Kokubu <ymymyummy@gmail.com>
2024-08-26 15:08:23 +0900

    put apiの移行

--- app/api/notion/route.ts
+++ app/api/notion/route.ts
@@ -3,37 +3,37 @@ export const runtime = "nodejs";
 
 import { Client } from "@notionhq/client";
 import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import { NextRequest } from "next/server";
+
 const notion = new Client({ auth: process.env.NOTION_API_TOKEN });
 
-export async function GET(request: Request) {
+export async function GET(_request: NextRequest) {
   const latestPage = await getLatestPageFromDatabase();
+  const { url: latestPageUrl, id: latestPageId } = latestPage;
+
   return new Response(
-    JSON.stringify({ status: "success", result: latestPage.url })
+    JSON.stringify({ status: "success", result: latestPageUrl })
   );
 }
 
-// async function doPost(e) {
-//   const data = JSON.parse(e.postData.contents);
-//   const text = data.text;
+export async function PUT(request: NextRequest) {
+  const data = await request.json();
+  const text = data.text;
 
-//   if (!text) {
-//     return JSON.stringify({ status: "error", message: "No text provided" });
-//   }
+  if (!text) {
+    return new Response(
+      JSON.stringify({ status: "error", message: "No text provided" })
+    );
+  }
 
-//   const latestPage = getLatestPageFromDatabase();
-//   const latestPageId = await latestPage?.id;
-//   if (latestPageId) {
-//     const result = appendTextToPage(latestPageId, text);
-//     return JSON.stringify({ status: "success", result: result });
-//   } else {
-//     return JSON.stringify({
-//       status: "error",
-//       message: "No pages found in the database.",
-//     });
-//   }
-// }
+  const latestPage = await getLatestPageFromDatabase();
+  const { id: latestPageId } = latestPage;
 
-async function getLatestPageFromDatabase(): Promise<PageObjectResponse> {
+  const result = await appendTextToPage(latestPageId, text);
+  return new Response(JSON.stringify({ status: "success", result: result }));
+}
+
+const getLatestPageFromDatabase = async (): Promise<PageObjectResponse> => {
   if (!process.env.NOTION_DATABASE_ID) {
     throw new Error("No database ID provided");
   }
@@ -54,36 +54,27 @@ async function getLatestPageFromDatabase(): Promise<PageObjectResponse> {
   }
 
   return latestPage as PageObjectResponse;
-}
+};
 
-// async function appendTextToPage(pageId: string, text = "test") {
-//   const url = `https://api.notion.com/v1/blocks/${pageId}/children`;
-//   const options = {
-//     method: "patch",
-//     headers: {
-//       Authorization: `Bearer ${NOTION_API_KEY}`,
-//       "Content-Type": "application/json",
-//       "Notion-Version": "2022-06-28",
-//     },
-//     payload: JSON.stringify({
-//       children: [
-//         {
-//           object: "block",
-//           type: "bulleted_list_item",
-//           bulleted_list_item: {
-//             rich_text: [
-//               {
-//                 type: "text",
-//                 text: {
-//                   content: text,
-//                 },
-//               },
-//             ],
-//           },
-//         },
-//       ],
-//     }),
-//   };
-//   const response = await fetch(url, options);
-//   console.log(response.getContentText());
-// }
+const appendTextToPage = async (pageId: string, text = "test") => {
+  const response = await notion.blocks.children.append({
+    block_id: pageId,
+    children: [
+      {
+        object: "block",
+        type: "bulleted_list_item",
+        bulleted_list_item: {
+          rich_text: [
+            {
+              type: "text",
+              text: {
+                content: text,
+              },
+            },
+          ],
+        },
+      },
+    ],
+  });
+  return response;
+};

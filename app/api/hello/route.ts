d2b427cd3d2ea19d9ede1d04de1f773ff5cb6e3c
Yumiko Kokubu <ymymyummy@gmail.com>
2024-08-26 10:40:12 +0900

    test2

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -3,6 +3,6 @@ export const runtime = "nodejs";
 
 export function GET(request: Request) {
   return new Response(
-    `Hello from ${process.env.VERCEL_REGION} ${process.env.TEST_ENV}!!!`
+    `Hello from ${process.env.VERCEL_REGION} ${process.env.TEST_ENV}!!!!!!`
   );
 }

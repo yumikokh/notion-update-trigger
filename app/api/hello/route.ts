5022663551d459134f196b023252a94d5a7d838e
Yumiko Kokubu <ymymyummy@gmail.com>
2024-08-26 10:22:00 +0900

    first commit

---
+++ app/api/hello/route.ts
@@ -0,0 +1,8 @@
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
+export function GET(request: Request) {
+  return new Response(
+    `Hello from ${process.env.VERCEL_REGION} ${process.env.TEST_ENV}`
+  );
+}
